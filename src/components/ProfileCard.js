d02736a8ee1c004db2f9f9ff021011a9da50573d
Dumba-Theresa03 <dumbatheresa3@gmail.com>
2022-03-23 12:53:46 +0000

    added usestate

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,20 +1,36 @@
 import React from 'react'
+import {useState} from "react"
 
 function ProfileCard({writer}) {
-    return (
-        <div className='card'>
-            <img src= {`images/${writer.avatar}.png`}
-            height="300px" width="300px" alt={writer.img}/>
+  const [showBio, setShowBio] = useState(false);
 
-            <div className='textGroup'>
-                <h3>{writer.name}</h3>
-                <p>{writer.email}</p>
-                <p>{writer.phone}</p>
+  const handleClick = (bioData) =>{
+    setShowBio(!showBio);
+  };
 
-                <button className='actionBtn'>Read Bio</button>
-            </div>
-        </ div>
-    )
+  return (
     
+<div className='card'>
+<div className='cardContent'>
+{showBio ? (
+  <div className='biowrap'>
+  <p className='bio'>{writer.bio}</p>  
+  </div>
+    ):(
+    <img src= {`images/${writer.avatar}.png`}
+  height="300px" width="300px" alt={writer.img}/>
+  )}
+  </div>
+
+<div className='textGroup'>
+<h3>{writer.name}</h3>
+<p>{writer.email}</p>
+<p>{writer.phone}</p>
+<button className='actionBtn' onClick={()=>handleClick(writer.bio)}>Read Bio</button>
+</div>
+</div>
+ 
+)
 }
-export default ProfileCard;
+
+export default ProfileCard
